f4290bdb1a7efdff31bcae1109c783c74d63aa93
KF5CVK <dustin.keefer@mavs.uta.edu>
2016-03-28 22:05:52 -0500

    Test 2 with added framework for lower level classes

---
+++ Robbie_Robot_Shop/src/robbie_robot_shop/Locomotor.java
@@ -0,0 +1,13 @@
+/*
+ * Dustin Craig Keefer - 1000917479
+ * CSE 1325 - Object Oriented Programming
+ */
+package robbie_robot_shop;
+
+/**
+ *
+ * @author dusti
+ */
+public class Locomotor {
+    
+}
