4c92c400bc5e7961c7efedc692d39389d888549c
jxk8128 <jonathanskortu@yahoo.com>
2016-03-30 21:32:19 -0500

    Added files via upload

--- Robbie_Robot_Shop/src/robbie_robot_shop/Arm.java
+++ Robbie_Robot_Shop/src/robbie_robot_shop/Arm.java
@@ -6,9 +6,22 @@ package robbie_robot_shop;
  *
  */
 public class Arm extends Component{
+    String myName = "Arm prototype";
+    
+    public Arm()
+    {
+        super();   
+    }
     
     int powerConsumed(int speed){
         int totalConsumed = 0;
         return totalConsumed;
+    }
+    
+    
+    @Override
+    void printSpecs()
+    {
+        System.out.println(this.myName);
     }
 }
