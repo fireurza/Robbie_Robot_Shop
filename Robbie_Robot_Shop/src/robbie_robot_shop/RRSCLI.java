39bfa63cff9e3d0a1391a85c8bfd5fc2a188075a
KF5CVK <dustin.keefer@mavs.uta.edu>
2016-04-02 17:15:06 -0500

    More Menu Navigation

--- Robbie_Robot_Shop/src/robbie_robot_shop/RRSCLI.java
+++ Robbie_Robot_Shop/src/robbie_robot_shop/RRSCLI.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 
 /*
  */
-public class CLI {
+public class RRSCLI {
     
     /*
-    * Print a brief explanation of navigating through the CLI
+    * Print a brief explanation of navigating through the RRSCLI
     */
     private static void printHelp() {
          System.out.println("Type the number next to the desired optionto select an option");
@@ -18,7 +18,7 @@ public class CLI {
     
     private static void printTitle() {
         System.out.println("========================");
-        System.out.println("Welcome to Robbie Robots");
+        System.out.println("     Robbie Robots");
         System.out.println("========================");
     }
     
@@ -47,11 +47,46 @@ public class CLI {
         printTitle();
         System.out.println("==========SHOP==========");
         System.out.println("");
-        System.out.println("1 - Create Part");
-        System.out.println("2 - Design Model");
-        System.out.println("3 - Browse Catalog");
+        System.out.println("1 - Parts");
+        System.out.println("2 - Models");
+        System.out.println("3 - Orders");
         System.out.println("4 - Exit");
         System.out.println("");
+        printHelp();
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        sc.close();
+        return input;
+        }
+    
+    private static int part(){
+        clearScreen();
+        printTitle();
+        System.out.println("==========PART==========");
+        System.out.println("");
+        System.out.println("1 - Create New Part");
+        System.out.println("2 - Edit Old Part");
+        System.out.println("3 - Browse Parts");
+        System.out.println("4 - Exit");
+        System.out.println("");
+        printHelp();
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        sc.close();
+        return input;
+        }
+    
+    private static int model(){
+        clearScreen();
+        printTitle();
+        System.out.println("==========PART==========");
+        System.out.println("");
+        System.out.println("1 - Create New Model");
+        System.out.println("2 - Edit Old Model");
+        System.out.println("3 - Browse Models");
+        System.out.println("4 - Exit");
+        System.out.println("");
+        printHelp();
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
         sc.close();
@@ -60,6 +95,7 @@ public class CLI {
     
     public static void main(String[] args) {
         clearScreen();
+        
         // Display menu 1
         int input = menu1();
         boolean done = false;
@@ -70,27 +106,39 @@ public class CLI {
             // Call Shop Function
             int input2 = shop();
             if (input2 == 1){
-                // Create part
+                int input3 = part();// Create part
+                if (input3 == 1){
+                    // Create New Part
+                }
+                if (input3 == 2){
+                    
+                }
+                if (input3 == 3){
+                    
+                }
+                if (input3 == 4){
+                    
+                }
             }
-            if (input2 == 2){
+            else if (input2 == 2){
                 // Design Model
             }
-            if (input2 == 3){
+            else if (input2 == 3){
                 // Browse Catalog
             }
-            if (input2 == 4){
+            else if (input2 == 4){
                 // exit shopping menu
                 done = true;
             }
             else {
                 System.out.println("Unrecognized input");
-                
             }
             }while (!done);
         }
         else if(input == 2){
             clearScreen();
-            // Exit Program Function
+            // Exit Program Function - Save any data and exit gracefully
+            
             return;
         }
         else {
--- Robbie_Robot_Shop/src/robbie_robot_shop/Shop.java
+++ Robbie_Robot_Shop/src/robbie_robot_shop/Shop.java
@@ -7,6 +7,7 @@ package robbie_robot_shop;
 public class Shop {
      
     Order[] orders=new Order[10000];
+    Component[] robotParts=new Component[100];
     RobotModel[] robotModels=new RobotModel[100];
     Customer[] customers=new Customer[100];
     SalesAssociate[] salesAssociates=new SalesAssociate[100];
@@ -14,6 +15,8 @@ public class Shop {
     public Shop(){
          
      }   
+    
+    void createRobotPart(){}
     
     void createRobotModel(){}
     
