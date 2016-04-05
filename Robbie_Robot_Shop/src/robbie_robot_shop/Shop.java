db0c157873734e3fa6e894f0ce0f8a097bba17d5
jxk8128 <jonathanskortu@yahoo.com>
2016-04-04 20:04:23 -0500

    Added files via upload
    Updated Shop.java
    Created a public static 2D Array of components
    Adjusted all createParts() methods to add newly created parts to Array of components

--- Robbie_Robot_Shop/src/robbie_robot_shop/Shop.java
+++ Robbie_Robot_Shop/src/robbie_robot_shop/Shop.java
@@ -1,4 +1,6 @@
 package robbie_robot_shop;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  *
@@ -6,11 +8,25 @@ package robbie_robot_shop;
  */
 public class Shop {
      
-    Order[] orders=new Order[10000];
-    Component[] robotParts=new Component[100];
-    RobotModel[] robotModels=new RobotModel[100];
-    Customer[] customers=new Customer[100];
-    SalesAssociate[] salesAssociates=new SalesAssociate[100];
+    public Order[] orders=new Order[10000];
+    // Containers for parts
+    private static Head[] HeadArray = new Head[100];
+    private static Torso[] TorsoArray = new Torso[100];
+    private static Arm[] ArmArray = new Arm[100];
+    private static Battery[] BatteryArray = new Battery[100];
+    private static Locomotor[] LocomotorArray = new Locomotor[100];
+    public static Component[][] robotParts = {HeadArray, TorsoArray, ArmArray, BatteryArray, LocomotorArray};
+    
+    // Counters to keep track of array indexes
+    private static int headCounter = 0;
+    private static int torsoCounter = 0;
+    private static int armCounter = 0;
+    private static int batteryCounter = 0;
+    private static int locomotorCounter = 0;
+    
+    public RobotModel[] robotModels=new RobotModel[100];
+    public Customer[] customers=new Customer[100];
+    public SalesAssociate[] salesAssociates=new SalesAssociate[100];
     
     // Constructor
     public Shop(){
@@ -19,7 +35,7 @@ public class Shop {
     
     // Utility methods
     
-    public static Torso createTorso(String Tname, double Tweight, double Tprice, String Tdescription, boolean Tobsolete, boolean Tapproved, int TbatteryCompartments, int TarmSockets){
+    public static void createTorso(String Tname, double Tweight, double Tprice, String Tdescription, boolean Tobsolete, boolean Tapproved, int TbatteryCompartments, int TarmSockets){
         Torso T1 = new Torso();
         T1.setType("Torso");
         T1.setName(Tname);
@@ -30,9 +46,9 @@ public class Shop {
         T1.setApproved(Tapproved);
         T1.setBatteryCompartments(TbatteryCompartments);
         T1.setArmSockets(TarmSockets);
-        return T1;
+        TorsoArray[torsoCounter++] = T1;
     }
-    public static Locomotor createLocomotor(String Lname, double Lweight, double Lprice, String Ldescription, boolean Lobsolete, boolean Lapproved, double LmaxSpeed, double LpowerConsumptionRate){
+    public static void createLocomotor(String Lname, double Lweight, double Lprice, String Ldescription, boolean Lobsolete, boolean Lapproved, double LmaxSpeed, double LpowerConsumptionRate){
         Locomotor L1 = new Locomotor();
         L1.setType("locomotor");
         L1.setName(Lname);
@@ -43,9 +59,9 @@ public class Shop {
         L1.setApproved(Lapproved);
         L1.setMaxSpeed(LmaxSpeed);
         L1.setPowerConsumptionRate(LpowerConsumptionRate);
-        return L1;
+        LocomotorArray[locomotorCounter++] = L1;
     }
-    public static Battery createBattery(String Bname, double Bweight, double Bprice, String Bdescription, boolean Bobsolete, boolean Bapproved, double Benergy, double BmaxPower){
+    public static void createBattery(String Bname, double Bweight, double Bprice, String Bdescription, boolean Bobsolete, boolean Bapproved, double Benergy, double BmaxPower){
         Battery B1 = new Battery();
         B1.setType("battery");
         B1.setName(Bname);
@@ -56,9 +72,9 @@ public class Shop {
         B1.setApproved(Bapproved);
         B1.setEnergy(Benergy);
         B1.setMaxPower(BmaxPower);
-        return B1;
+        BatteryArray[batteryCounter++] = B1;
     }
-    public static Head createHead(String Hname, double Hweight, double Hprice, String Hdescription, boolean Hobsolete, boolean Happroved){
+    public static void createHead(String Hname, double Hweight, double Hprice, String Hdescription, boolean Hobsolete, boolean Happroved){
         Head H1 = new Head();
         H1.setType("head");
         H1.setName(Hname);
@@ -67,9 +83,9 @@ public class Shop {
         H1.setDescription(Hdescription);
         H1.setObsolete(Hobsolete);
         H1.setApproved(Happroved);
-        return H1;
+        HeadArray[headCounter++] = H1;
     }
-    public static Arm createArm(String Aname, double Aweight, double Aprice, String Adescription, boolean Aobsolete, boolean Aapproved, double ApowerConsumptionRate){
+    public static void createArm(String Aname, double Aweight, double Aprice, String Adescription, boolean Aobsolete, boolean Aapproved, double ApowerConsumptionRate){
         Arm A1 = new Arm();
         A1.setType("arm");
         A1.setName(Aname);
@@ -79,7 +95,7 @@ public class Shop {
         A1.setObsolete(Aobsolete);
         A1.setApproved(Aapproved);
         A1.setPowerConsumptionRate(ApowerConsumptionRate);
-        return A1;
+        ArmArray[armCounter++] = A1;
     }
     
     public static void editPart(){
