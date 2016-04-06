1815708586bc128a8ced91952e9784a9b428c70a
KF5CVK <dustin.keefer@mavs.uta.edu>
2016-04-06 16:45:22 -0500

    Shop Update - added all setters for creation of new parts

--- Robbie_Robot_Shop/src/robbie_robot_shop/RRSCLIModelManager.java
+++ Robbie_Robot_Shop/src/robbie_robot_shop/RRSCLIModelManager.java
@@ -11,11 +11,56 @@ package robbie_robot_shop;
 public class RRSCLIModelManager {
     
     public static void modelManager(){
-        
+        boolean exit = false;
+        do{
+        RRSCLI.clearScreen();
+        RRSCLI.printTitle();
+        System.out.println("======Model Manager=====");
+        System.out.println("");
+        System.out.println("(S)earch Models");
+        System.out.println("(B)rowse Models");
+        System.out.println("(E)dit Model");
+        System.out.println("(N)ew Model");
+        System.out.println("E(x)it");
+        int input = CLIinput.getChar(" Select Option ");
+        if (input == 'S' || input == 's'){
+            searchModels();
+        }
+        if (input == 'B' || input == 'b'){
+            browseModels();
+        }
+        else if (input == 'E' || input == 'e'){
+            editModel();
+        }
+        else if (input == 'N' || input == 'n'){
+            newModel();
+        }
+        else if (input == 'X' || input == 'x'){
+            exit = true;
+        }
+        else{
+            System.out.println("Unrecognized Input");
+        }
+        }while(!exit);
     }
     
     public static void newModel(){
-        
+        boolean exit = false;
+        do{
+        RRSCLI.clearScreen();
+        RRSCLI.printTitle();
+        System.out.println("====New Model Creator====");
+        System.out.println("");
+        System.out.println("Choose (T)orso by entering it's part number");
+        System.out.println("E(x)it");
+        int input = CLIinput.getChar(" Select Option ");
+        if (input == 'X' || input == 'x'){
+            exit = true;
+        }
+        else{
+            System.out.println("Unrecognized Input");
+        }
+        }while(!exit);
     }
     
     public static void editModel(){
@@ -26,7 +71,7 @@ public class RRSCLIModelManager {
         
     }
     
-    public static void searchModel(){
+    public static void searchModels(){
         
     }
     
