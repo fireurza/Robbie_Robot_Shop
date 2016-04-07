7399b1a9b7acca8e22ccabbe0d51d2d4449c0c85
Dustin <dustin.keefer83@gmail.com>
2016-04-07 03:43:41 -0500

    Sync between Class Diagram and Current Classes

--- Robbie_Robot_Shop/src/robbie_robot_shop/Customer.java
+++ Robbie_Robot_Shop/src/robbie_robot_shop/Customer.java
@@ -1,19 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package robbie_robot_shop;
 
-/**
- *
- * @author Gasser
- */
+
 public class Customer {
      private String name;  
      private int customerNumber=1; 
      private double bill;
-     Order[] orders=new Order[10000];
+//     Order[] orders=new Order[10000];
      
      public Customer(String name){
      this.name=name;
@@ -21,7 +14,19 @@ public class Customer {
      this.bill=0;
      }
      
+     public void setName(String name){
+         this.name = name;
+     }
      
+     public String getName(){
+         return this.name;
+     }
      
+     public void setBill(double bill){
+         this.bill = bill;
+     }
      
+     public double getBill(){
+         return this.bill;
+     }
 }
